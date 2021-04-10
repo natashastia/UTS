a60988f916ab68c17bc09b6d25b490141d63b164
Natasha Astia <natashaastia@mail.ugm.ac.id>
2021-04-10 07:18:14 +0700

    commit pertama

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var book = new Book ("My GradeBook");
+
+            while (true)
+            {
+                Console.WriteLine("Enter a grade or 'q' to quit: ");
+                var input = Console.ReadLine();
+                if (input == "q")
+                {
+                    break;
+                }
+
+                try
+                {
+                    var grade = double.Parse(input);
+                    book.AddGrade(grade);
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.WriteLine("***");
+                }
+            }
+
+            var stats = book.GetStatistics();
+            Console.WriteLine($"For the book named {book.Name}");
+            book.Name = "";
+            Console.WriteLine($"The lowest grade is {stats.Low}");
+            Console.WriteLine($"The highest grade is {stats.High}");
+            Console.WriteLine($"The average grade is {stats.Ave : N2}");
+            Console.WriteLine($"The letter grade is {stats.Letter}");
+        }
+    }
+}
---
+++ Statistics.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook 
+{
+    public class Statistics
+    {
+        public double Ave;
+        public double High;
+        public double Low;
+        public char Letter;
+    }
+}
